824709e11f66ea4a6c0894271d0c2934560279ae
Joey Harig <jharig@esri.com>
2021-09-20 09:08:04 -0600

    Change app name

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,15 +7,13 @@
     }"
   >
     <!-- Logo and Title -->
-    <div class="flex align-center">
+    <div v-if="$store.state.screen_size !== 'xs'" class="flex align-center">
       <img class="header-logo margin-right-half" src="@/assets/nrcs-logo.png" />
-      <div v-if="$store.state.screen_size !== 'xs'">
+      <div>
         <h1 class="margin-0 fz--1 margin-bottom-quarter demi">
-          {{
-            `${
-              $store.state.state ? $store.state.state.name : ""
-            } Climate and Water Report`
-          }}
+          
+            Water Supply Outlook App
+          
         </h1>
         <h2
           class="margin-0 fz--2 margin-bottom-quarter text-light text-regular"
@@ -25,54 +23,90 @@
       </div>
     </div>
 
-    <div v-if="$store.state.state && $store.state.view" class="align-center">
-      <div class="header-button-group margin-right-1 padding-right-1">
-        <!-- View Toggle Small Screens -->
-        <calcite-dropdown
-          v-if="$store.state.screen_size !== 'm'"
-          placement="bottom-trailing"
-        >
-          <calcite-button
-            slot="dropdown-trigger"
-            appearance="clear"
-            scale="s"
-            icon-end="chevron-down"
-          >
-            Select Metric
-          </calcite-button>
-          <calcite-dropdown-group selection-mode="single" role="menu">
-            <calcite-dropdown-item
-              v-for="view in $store.state.views"
-              :key="view.name"
-              :disabled="$store.state.status === 'PENDING'"
-              :active="$store.state.view.name === view.name"
-              @click="$store.commit('view', view)"
+    <div
+      v-if="$store.state.state && $store.state.view"
+      class="align-center"
+      :class="{ 'full-width space-between': $store.state.screen_size === 'xs' }"
+    >
+      <div
+        class="margin-right-1 padding-right-1 align-center"
+        :class="{ 'header-button-group': $store.state.screen_size !== 'xs' }"
+      >
+        <!-- State Toggle -->
+        <div class="align-center margin-right-half">
+          <span class="fz--2 margin-right-quarter">State:</span>
+          <calcite-dropdown placement="bottom-trailing">
+            <calcite-button
+              slot="dropdown-trigger"
+              appearance="clear"
+              scale="s"
+              icon-end="chevron-down"
             >
-              {{ view.name }}
-            </calcite-dropdown-item>
-          </calcite-dropdown-group>
-        </calcite-dropdown>
+              {{ $store.state.state.name }}
+            </calcite-button>
+            <calcite-dropdown-group
+              selection-mode="single"
+              role="menu"
+              group-title="Select State"
+            >
+              <calcite-dropdown-item
+                v-for="state in $store.state.states"
+                :key="state.name"
+                :disabled="$store.state.status === 'PENDING'"
+                :active="$store.state.state.name === state.name"
+                @click="$store.commit('state', state)"
+              >
+                {{ state.name }}
+              </calcite-dropdown-item>
+            </calcite-dropdown-group>
+          </calcite-dropdown>
+        </div>
 
-        <!-- View Toggle Large Screens -->
-        <calcite-button
-          v-else
-          v-for="view in $store.state.views"
-          :key="view.name"
-          :disabled="$store.state.status === 'PENDING'"
-          :appearance="
-            $store.state.view.name === view.name ? 'solid' : 'transparent'
-          "
-          @click="$store.commit('view', view)"
-          scale="s"
-        >
-          {{ view.name }}
-        </calcite-button>
+        <!-- View Toggle -->
+        <div class="align-center">
+          <span class="fz--2 margin-right-quarter">Map:</span>
+          <calcite-dropdown placement="bottom-trailing">
+            <calcite-button
+              slot="dropdown-trigger"
+              appearance="clear"
+              scale="s"
+              icon-end="chevron-down"
+            >
+              {{ $store.state.view.name }}
+            </calcite-button>
+            <calcite-dropdown-group
+              selection-mode="single"
+              role="menu"
+              group-title="Select Map"
+            >
+              <calcite-dropdown-item
+                v-for="view in $store.state.views"
+                :key="view.name"
+                :disabled="$store.state.status === 'PENDING'"
+                :active="$store.state.view.name === view.name"
+                @click="$store.commit('view', view)"
+              >
+                {{ view.name }}
+              </calcite-dropdown-item>
+            </calcite-dropdown-group>
+          </calcite-dropdown>
+        </div>
       </div>
 
       <!-- Download Btn -->
-      <calcite-button scale="s" appearance="clear">
+      <calcite-button
+        v-if="$store.state.screen_size !== 'xs'"
+        scale="s"
+        @click="downloadReport"
+      >
         Download Monthly Report
       </calcite-button>
+      <calcite-button
+        v-else
+        scale="s"
+        icon-end="download"
+        @click="downloadReport"
+      />
     </div>
   </header>
 </template>
@@ -87,7 +121,11 @@ export default {
   },
   watch: {},
   computed: {},
-  methods: {},
+  methods: {
+    downloadReport() {
+      window.open(this.$store.state.state.link_to_download_reports, "_blank");
+    },
+  },
 };
 </script>
 
