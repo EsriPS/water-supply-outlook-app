57da48d60ca1ff9fe53bb29ff832a507e09a32c6
Joey Harig <jharig@esri.com>
2022-07-22 10:42:14 -0600

    remove console logs

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -98,19 +98,20 @@ export default {
       filterSitesByState: () => {},
 
       // Legend
-      isLegendVisible: this.$store.state.screen_size !== "xs",
+      isLegendVisible:
+        this.$store.state.screen_size !== "xs",
       toggleLegend: () => {},
 
       // Layer List
       isLayerListVisible: false,
-      toggleLayerList: () => {}
+      toggleLayerList: () => {},
     };
   },
   watch: {
     "metric.map_id": {
       handler() {
         this.renderMap();
-      }
+      },
     },
     feature: {
       deep: true,
@@ -122,16 +123,17 @@ export default {
           this.filterBasinsByState();
           this.filterSitesByState();
         }
-      }
+      },
     },
     "$store.state.screen_size": {
       handler(val) {
         if (val === "xs") {
           if (this.isLegendVisible) this.toggleLegend();
-          if (this.isLayerListVisible) this.toggleLayerList();
+          if (this.isLayerListVisible)
+            this.toggleLayerList();
         }
-      }
-    }
+      },
+    },
   },
   computed: {},
   methods: {
@@ -142,210 +144,236 @@ export default {
           "esri/views/MapView",
           "esri/widgets/Legend",
           "esri/layers/FeatureLayer",
-          "esri/widgets/LayerList"
+          "esri/widgets/LayerList",
         ],
         { css: true }
-      ).then(async ([WebMap, MapView, Legend, FeatureLayer, LayerList]) => {
-        // Load features
-        if (!this.$store.state.features.length) {
-          try {
-            const layer = new FeatureLayer({
-              portalItem: {
-                id: this.$store.state[`basins_feature_layer_id`]
-              }
-            });
-            const { features } = await layer.queryFeatures();
-            // console.log(features);
-            this.$store.commit("updatedAt", features[0]);
-            this.$store.commit("features", features);
-            this.$store.commit("status", "OK");
-          } catch (error) {
-            console.log("query failed: ", error);
-            this.$store.commit("status", "ERROR");
-          }
-        }
-
-        // Fetch Correct WebMap
-        const map = new WebMap({
-          portalItem: {
-            id: this.metric.map_id
-          }
-        });
-
-        // Define View
-        const view = new MapView({
-          map,
-          container: "viewDiv",
-          center: this.state.center,
-          zoom: this.state.zoom
-        });
-
-        // Allow Vue to navigate map view
-        this.goTo = () => {
-          console.log("go to:", this.feature);
-          view.goTo(
-            this.feature
-              ? this.feature
-              : {
-                  center: this.state.center,
-                  zoom: this.state.zoom
+      ).then(
+        async ([
+          WebMap,
+          MapView,
+          Legend,
+          FeatureLayer,
+          LayerList,
+        ]) => {
+          // Load features
+          if (!this.$store.state.features.length) {
+            try {
+              const layer = new FeatureLayer({
+                portalItem: {
+                  id: this.$store.state[
+                    `basins_feature_layer_id`
+                  ],
                 },
-            {
-              duration: this.feature ? 500 : 1000
+              });
+              const {
+                features,
+              } = await layer.queryFeatures();
+              this.$store.commit("updatedAt", features[0]);
+              this.$store.commit("features", features);
+              this.$store.commit("status", "OK");
+            } catch (error) {
+              console.log("query failed: ", error);
+              this.$store.commit("status", "ERROR");
             }
-          );
-        };
-
-        view.when(() => {
-          const visible = this.$store.state.screen_size !== "xs";
-
-          // Add Legend
-          const legend = new Legend({
-            view,
-            visible,
-            layerInfos: map.allLayers.items
-              .filter(layer => layer.type == "feature")
-              .map(layer => {
-                return {
-                  layer
-                };
-              })
-          });
-          view.ui.add(legend, "bottom-left");
-
-          // Add Layer Toggle Legend
-          const layerList = new LayerList({
-            view,
-            visible: false
-          });
-          view.ui.add(layerList, "bottom-left");
-
-          // Allow vue to control legend
-          this.toggleLegend = () => {
-            this.isLegendVisible = !this.isLegendVisible;
-            legend.visible = !legend.visible;
-            if (this.$store.state.screen_size === "xs") {
-              layerList.visible = false;
-              this.isLayerListVisible = false;
-            }
-          };
-
-          // Allow vue to control layer list
-          this.toggleLayerList = () => {
-            this.isLayerListVisible = !this.isLayerListVisible;
-            layerList.visible = !layerList.visible;
-
-            if (this.$store.state.screen_size === "xs") {
-              legend.visible = false;
-              this.isLegendVisible = false;
-            }
-          };
-
-          // Get stations feature layer
-          const sitesLayer = map.layers.find(l =>
-            ["Sites", "Reservoirs"].includes(l.title)
-          );
-          view.whenLayerView(sitesLayer).then(layerView => {
-            // Allow vue to filter features by state
-            this.filterSitesByState = () => {
-              layerView.filter = {
-                where: `
-                      stationTri LIKE '%${this.state.code}%'
-                    `
-              };
-            };
-            this.filterSitesByState();
-          });
-
-          // Get basins layer
-          const basinsLayer = map.layers.find(l => l.title === "Basins");
-
-          // Add basin layer class breaks to store
-          if (this.metric.classBreaks) {
-            this.$store.commit("classBreaks", {
-              metric: this.metric,
-              classBreaks: basinsLayer?.renderer?.classBreakInfos
-            });
           }
 
-          view
-            .whenLayerView(basinsLayer)
-            .then(layerView => {
-              // Allow vue to filter features by state
-              this.filterBasinsByState = () => {
-                layerView.filter = {
-                  where: `
-                      btype = '${this.state.basin_huc_code}'
-                    `
+          // Fetch Correct WebMap
+          const map = new WebMap({
+            portalItem: {
+              id: this.metric.map_id,
+            },
+          });
+
+          // Define View
+          const view = new MapView({
+            map,
+            container: "viewDiv",
+            center: this.state.center,
+            zoom: this.state.zoom,
+          });
+
+          // Allow Vue to navigate map view
+          this.goTo = () => {
+            view.goTo(
+              this.feature
+                ? this.feature
+                : {
+                    center: this.state.center,
+                    zoom: this.state.zoom,
+                  },
+              {
+                duration: this.feature ? 500 : 1000,
+              }
+            );
+          };
+
+          view.when(() => {
+            const visible =
+              this.$store.state.screen_size !== "xs";
+
+            // Add Legend
+            const legend = new Legend({
+              view,
+              visible,
+              layerInfos: map.allLayers.items
+                .filter((layer) => layer.type == "feature")
+                .map((layer) => {
+                  return {
+                    layer,
+                  };
+                }),
+            });
+            view.ui.add(legend, "bottom-left");
+
+            // Add Layer Toggle Legend
+            const layerList = new LayerList({
+              view,
+              visible: false,
+            });
+            view.ui.add(layerList, "bottom-left");
+
+            // Allow vue to control legend
+            this.toggleLegend = () => {
+              this.isLegendVisible = !this.isLegendVisible;
+              legend.visible = !legend.visible;
+              if (this.$store.state.screen_size === "xs") {
+                layerList.visible = false;
+                this.isLayerListVisible = false;
+              }
+            };
+
+            // Allow vue to control layer list
+            this.toggleLayerList = () => {
+              this.isLayerListVisible = !this
+                .isLayerListVisible;
+              layerList.visible = !layerList.visible;
+
+              if (this.$store.state.screen_size === "xs") {
+                legend.visible = false;
+                this.isLegendVisible = false;
+              }
+            };
+
+            // Get stations feature layer
+            const sitesLayer = map.layers.find((l) =>
+              ["Sites", "Reservoirs"].includes(l.title)
+            );
+            view
+              .whenLayerView(sitesLayer)
+              .then((layerView) => {
+                // Allow vue to filter features by state
+                this.filterSitesByState = () => {
+                  layerView.filter = {
+                    where: `
+                      stationTri LIKE '%${this.state.code}%'
+                    `,
+                  };
                 };
-              };
-
-              // Filter features by state
-              this.filterBasinsByState();
-
-              // Highlight Selected Feature
-              this.highlight = () => {
-                if (this.feature) {
-                  const where = `id = '${this.feature?.attributes.id}'`;
-                  layerView.effect = {
-                    includedEffect: this.metric.includedEffect,
-                    excludedEffect: this.metric.excludedEffect,
-                    filter: { where }
-                  };
-                } else {
-                  layerView.effect = {
-                    includedEffect: this.metric.baseEffect
-                  };
-                }
-              };
-
-              // Watch for user clicking on a feature
-              view.on("click", event => {
-                view.hitTest(event).then(event => {
-                  // Perform a hitTest on the View
-                  // Make sure graphic has a popupTemplate
-                  const results = event.results.filter(result => {
-                    return result.graphic.layer.popupTemplate;
-                  });
-                  const result = results[0];
-                  const OBJECTID = result?.graphic?.attributes.OBJECTID;
-                  const feature = this.features.find(
-                    f => f.attributes.OBJECTID === OBJECTID
-                  );
-                  const isNewFeature =
-                    OBJECTID !== this.feature?.attributes.OBJECTID;
-                  // If we find a basin, update the focussed feature
-                  if (feature && isNewFeature) {
-                    this.updateFeature(feature);
-                  }
-
-                  // If the user clicked outside all basins, zoom
-                  // back to state level
-                  else if (!results?.length) {
-                    this.clearFeature();
-                    this.goTo();
-                  }
-                });
+                this.filterSitesByState();
               });
 
-              // Zoom to feature if selected
-              if (this.feature) {
-                this.goTo(this.feature);
-                this.highlight();
-                this.getPopupContent();
-              }
+            // Get basins layer
+            const basinsLayer = map.layers.find(
+              (l) => l.title === "Basins"
+            );
 
-              this.isInitialized = true;
-            })
-            .catch(err => console.log(err));
-        });
-      });
-    }
+            // Add basin layer class breaks to store
+            if (this.metric.classBreaks) {
+              this.$store.commit("classBreaks", {
+                metric: this.metric,
+                classBreaks:
+                  basinsLayer?.renderer?.classBreakInfos,
+              });
+            }
+
+            view
+              .whenLayerView(basinsLayer)
+              .then((layerView) => {
+                // Allow vue to filter features by state
+                this.filterBasinsByState = () => {
+                  layerView.filter = {
+                    where: `
+                      btype = '${this.state.basin_huc_code}'
+                    `,
+                  };
+                };
+
+                // Filter features by state
+                this.filterBasinsByState();
+
+                // Highlight Selected Feature
+                this.highlight = () => {
+                  if (this.feature) {
+                    const where = `id = '${this.feature?.attributes.id}'`;
+                    layerView.effect = {
+                      includedEffect: this.metric
+                        .includedEffect,
+                      excludedEffect: this.metric
+                        .excludedEffect,
+                      filter: { where },
+                    };
+                  } else {
+                    layerView.effect = {
+                      includedEffect: this.metric
+                        .baseEffect,
+                    };
+                  }
+                };
+
+                // Watch for user clicking on a feature
+                view.on("click", (event) => {
+                  view.hitTest(event).then((event) => {
+                    // Perform a hitTest on the View
+                    // Make sure graphic has a popupTemplate
+                    const results = event.results.filter(
+                      (result) => {
+                        return result.graphic.layer
+                          .popupTemplate;
+                      }
+                    );
+                    const result = results[0];
+                    const OBJECTID =
+                      result?.graphic?.attributes.OBJECTID;
+                    const feature = this.features.find(
+                      (f) =>
+                        f.attributes.OBJECTID === OBJECTID
+                    );
+                    const isNewFeature =
+                      OBJECTID !==
+                      this.feature?.attributes.OBJECTID;
+                    // If we find a basin, update the focussed feature
+                    if (feature && isNewFeature) {
+                      this.updateFeature(feature);
+                    }
+
+                    // If the user clicked outside all basins, zoom
+                    // back to state level
+                    else if (!results?.length) {
+                      this.clearFeature();
+                      this.goTo();
+                    }
+                  });
+                });
+
+                // Zoom to feature if selected
+                if (this.feature) {
+                  this.goTo(this.feature);
+                  this.highlight();
+                  this.getPopupContent();
+                }
+
+                this.isInitialized = true;
+              })
+              .catch((err) => console.log(err));
+          });
+        }
+      );
+    },
   },
   mounted() {
     // Render map
     this.renderMap();
-  }
+  },
 };
 </script>
 
@@ -387,3 +415,4 @@ export default {
   display: none !important;
 }
 </style>
+
