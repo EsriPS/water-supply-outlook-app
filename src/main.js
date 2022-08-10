6fabf6d9ba3f9de840cec3ac20cc3500778a47a4
Joey Harig <jharig@esri.com>
2022-08-10 14:56:49 -0600

    fix calcite icons

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ import {
   applyPolyfills as applyPolyfillsC,
   defineCustomElements as defineCustomElementsC,
 } from "@esri/calcite-components/dist/loader";
-// import { setAssetPath } from "@esri/calcite-components/dist/custom-elements";
+import { setAssetPath } from "@esri/calcite-components/dist/custom-elements";
 
 // allow us to use calcite-components without direct imports
 applyPolyfillsC().then(() => {
@@ -29,7 +29,9 @@ applyPolyfillsC().then(() => {
 });
 
 // tell the browser wher to find assets
-// setAssetPath(location.href);
+setAssetPath(
+  "https://unpkg.com/@esri/calcite-components/dist/calcite/assets"
+);
 
 // tell Vue.js to ignore Calcite Components
 Vue.config.ignoredElements = [/calcite-\w*/];
